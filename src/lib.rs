f2c7ffe88a20167eb01d69d34967dd399a62fe8c
Yury Cavalcante <yurypcfarias@gmail.com>
2023-06-29 16:10:40 -0300

    test: testing characters and games endpoint

--- src/lib.rs
+++ src/lib.rs
@@ -52,7 +52,7 @@ impl APIWrapper {
       request_endpoint: &str
     ) -> Result<D>
     where
-        D: DeserializeOwned,
+      D: DeserializeOwned,
     {
         let response = self
           .http_client
@@ -66,7 +66,7 @@ impl APIWrapper {
 
     pub fn games<'a>(&'a self) -> EndpointHelper<'a>{
       EndpointHelper { wrapper: self, query_string: Vec::new(), endpoint: "games"}
-}
+    }
 
     pub fn characters<'a>(&'a self) -> EndpointHelper<'a>{
       EndpointHelper { wrapper: self, query_string: Vec::new(), endpoint: "characters"}
@@ -75,18 +75,39 @@ impl APIWrapper {
 
 #[cfg(test)]
 mod tests {
+  use super::*;
   use std::env;
-  use crate::{APIWrapper, Game};
 
   #[test]
-  fn zelda_games() {
+  fn search_games() {
     let access_token = env::var("TWITCH_ACCESS_TOKEN").unwrap();
     let client_id = env::var("TWITCH_CLIENT_ID").unwrap();
-
     let api_wrapper = APIWrapper::new(&access_token, &client_id).unwrap();
 
-    let zelda_games: Vec<Game> = api_wrapper.post().unwrap();
+    let zelda_games: Vec<Game> = api_wrapper.games().search("zelda").limit("2").fields("name").request().unwrap();
 
-    println!("{:?}", zelda_games);
+    let expected_result: Vec<Game> = vec![
+      Game { id: 1025, name: String::from("Zelda II: The Adventure of Link") },
+      Game { id: 1022, name: String::from("The Legend of Zelda") }
+    ];
+
+    assert_eq!(expected_result, zelda_games)
+  }
+
+  #[test]
+  fn search_characters() {
+    let access_token = env::var("TWITCH_ACCESS_TOKEN").unwrap();
+    let client_id = env::var("TWITCH_CLIENT_ID").unwrap();
+    let api_wrapper = APIWrapper::new(&access_token, &client_id).unwrap();
+
+    let mario_characters: Vec<Character> = api_wrapper.characters().search("mario").fields("name").request().unwrap();
+
+    let expected_result: Vec<Character> = vec![
+      Character { id: 3726, name: String::from("Mario") },
+      Character { id: 1564, name: String::from("Mario Alcalde") },
+      Character { id: 1112, name: String::from("Mario Auditore") }
+    ];
+
+    assert_eq!(expected_result, mario_characters)
   }
 }
