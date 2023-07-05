85f11f47e4a1caabcd2712c16a2783dabfc5fca6
Yury Cavalcante <yurypcfarias@gmail.com>
2023-07-05 20:40:14 -0300

    feat: add remaining genre attributes

--- src/models/genre.rs
+++ src/models/genre.rs
@@ -3,7 +3,10 @@ use serde::Deserialize;
 #[derive(Deserialize, Debug, PartialEq)]
 pub struct Genre {
   pub id: usize,
-  pub name: String,
+  pub checksum: Option<String>,
+  pub created_at: Option<usize>,
+  pub name: Option<String>,
   pub slug: Option<String>,
+  pub updated_at: Option<usize>,
+  pub url: Option<String>,
 }
-
