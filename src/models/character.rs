0bc195174e09ea7c1dc194490bb8095886216d0c
Yury Cavalcante <yurypcfarias@gmail.com>
2023-07-04 21:56:08 -0300

    chore: Added remaining attributes from character endpoint

--- src/models/character.rs
+++ src/models/character.rs
@@ -3,5 +3,19 @@ use serde::{ Deserialize };
 #[derive(Deserialize, Debug, PartialEq)]
 pub struct Character {
   pub id: usize,
-  pub name: String,
+  pub akas: Option<Vec<String>>,
+  pub checksum: Option<String>,
+  pub country_name: Option<String>,
+  pub created_at: Option<usize>,
+  pub description: Option<String>,
+  pub games: Option<Vec<usize>>,
+  pub gender: Option<u8>,
+  pub mug_shot: Option<usize>,
+  pub name: Option<String>,
+  pub slug: Option<String>,
+  pub species: Option<u8>,
+  pub updated_at: Option<usize>,
+  pub url: Option<String>,
+}
+
 }
