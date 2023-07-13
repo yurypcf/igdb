8c4dcb3ac27a8e3df69ae478763268937045ace1
Yury Cavalcante <yurypcfarias@gmail.com>
2023-07-13 14:09:29 -0300

    docs: generated comments to docs.rs

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,6 @@
+/*!
+    Module containing utility implementations for [crate::EndpointUtils] and API response handling.
+*/
+
 pub mod endpoint;
 pub mod response_handler;
-
-pub use endpoint::EndpointUtils;
